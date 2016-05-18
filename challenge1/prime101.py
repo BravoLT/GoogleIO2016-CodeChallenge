2fa81793e5d9144eeb1f4ee40009ead9f5a06a03
Tensei <todokeru.bluesky@gmail.com>
2016-05-18 10:54:00 -0400

    Update prime101.py

--- challenge1/prime101.py
+++ challenge1/prime101.py
@@ -3,8 +3,7 @@
 for i in range(102):
     count = 0
     for f in range(1,10):
-        if i % f == 0:
-            if i != f:
-                count += 1
-    if count < 2:
+        if i % f == 0 and i != f:
+            count += 1
+    if count < 2 and i != 1:
         print i
