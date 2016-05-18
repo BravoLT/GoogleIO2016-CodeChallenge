26f330e231596a8ec43d7121ac111fb05d941d0d
Kyle Smith <ksmith.developer@gmail.com>
2016-05-18 11:18:01 -0400

    Created VS solution and add solution to challenge 1.

---
+++ challenge1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace challenge1
+{
+    internal class Program
+    {
+        private static bool IsPrime(int number)
+        {
+            if (number % 2 == 0)
+                return false;
+
+            for (int i = 3; i * i <= number; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        private static void Main(string[] args)
+        {
+            for (int i = 1; i <= 101; i++)
+                Console.WriteLine($"{i}. Prime: {IsPrime(i)}");
+
+            Console.WriteLine("Press ENTER to exit.");
+            Console.ReadLine();
+        }
+    }
+}
