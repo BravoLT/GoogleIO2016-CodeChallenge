90bfbe6ae324233b9c79f7745034fa3fb641be41
aliitani <aliitani0@gmail.com>
2016-05-17 23:29:06 -0400

    Add files via upload

---
+++ challenge1/PrimeNumbers.java
@@ -0,0 +1,39 @@
+
+public class PrimeNumbers {
+	public static void main(String args[]) {
+
+		String result = "";
+		
+		for (int i = 2; i <= 101; i++) {
+
+			if (isPrime(i)) {
+
+				result += i + " ";
+
+			}
+
+		}
+		System.out.println(result);
+
+	}
+
+	public static boolean isPrime(int n) {
+
+		if (n == 2) {
+
+			return true;
+
+		}
+
+		for (int i = 2; i < (int) Math.sqrt(n) + 1; i++) {
+			
+			if (n % i == 0) {
+			
+				return false;
+			
+			}
+		}
+
+		return true;
+	}
+}
